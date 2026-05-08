bd3df1b71562ab6f8ce4d5038046a2f793650ae9
Zara Wang <zara.guan@gmail.com>
2026-05-08 17:56:36 -0400

    Deploy LCEA app

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/LCEA-app/",
   server: {
     open: true, // open browser when you run npm run dev
   },
