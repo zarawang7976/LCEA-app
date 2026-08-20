ac434112404a467c415e97c7f65b6143f6afe5f8
Zara Wang <zara.guan@gmail.com>
2026-08-20 04:42:27 -0400

    Serve the app at hipmetrics.org root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/LCEA-app/",
+  base: "/",
   server: {
     open: true, // open browser when you run npm run dev
   },
